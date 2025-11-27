5d1f912bec3dfba821d7b46c8122f57dc15ca064
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-11-27 21:02:23 +0100

    Changes

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import scipy.io as sio
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = sio.loadmat('data/dataset_meeting_room.mat')
 
-_ = data.get('dataset_CSI')
-_ = _[1, :, :]
+# _ = data.get('dataset_CSI')
+# _ = _[1, :, :]
 
-plt.plot(_)
-plt.show()
+# plt.plot(_)
+# plt.show()
+
+
+X = data.get('dataset_CSI')
+y = data.get('dataset_labels')
+
+print(X.shape)
+print(y.shape)
+
+x_means = X.mean(2)
+# print(x_means.shape)
+
+# plt.plot(x_means[3, :])
+# plt.show()
