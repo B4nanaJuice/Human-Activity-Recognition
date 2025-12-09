62d02484be2196315827732fa343a24885143ad6
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-12-09 22:40:15 +0100

    Show 4 plots

--- data_visualisation.py
+++ data_visualisation.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import json
-from sklearn.metrics import ConfusionMatrixDisplay
 
-## Det data from file
+## Overall data
 image_size = "64x64"
 conv_layers = 2
 fc_layers = 2
@@ -12,38 +12,64 @@ with open(f"results/{file_name}", 'r') as f:
     data = json.loads(f.read())
 
 num_runs = data["hyperparameters"]["num_runs"]
+runs = [_+1 for _ in range(num_runs)]
 
+num_epochs = data["hyperparameters"]["epochs"]
+epochs = [_+1 for _ in range(num_epochs)]
 
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+classes = ['bend', 'fall', 'lie down', 'run', 'sitdown', 'standup', 'walk']
 
-# Taille totale de la figure
-fig = plt.figure(figsize=(12, 6))
+## Create figures
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
-# Grille 2 x 3 (6 cases)
-gs = gridspec.GridSpec(2, 3, figure=fig, wspace=0.4, hspace=0.3)
+# Training time
+training_time = data["train"]["training_time"]
+average_tt = round(sum(training_time) / num_runs, 2)
 
-ax1 = fig.add_subplot(gs[:, 0])
-ax1.matshow(data["test"]["confusion_matrix"])
+ax1.plot(runs, training_time, '-', runs, [average_tt] * num_runs, '--')
+ax1.set_xlabel('Run index')
+ax1.set_ylabel('Time taken (s)')
+ax1.set_title('Training time')
+ax1.legend(['Measurments', f'Average ({average_tt}s)'])
+ax1.grid(visible = True)
 
-training_time_mean = round(sum(data["train"]["training_time"]) / num_runs, 2)
-ax2 = fig.add_subplot(gs[0, 1])
-ax2.plot([_+1 for _ in range(num_runs)], data["train"]["training_time"], "-", [_+1 for _ in range(num_runs)], [training_time_mean]*num_runs, "--")
-ax2.set_xlabel('Run')
-ax2.set_ylabel('Training time (s)')
-ax2.set_title('Training part')
-ax2.legend(['Measurments', f'Mean ({training_time_mean}s)'])
+# Training loss
+losses = data["train"]["losses"]
+for _ in range(num_runs):
+    ax2.semilogy(epochs, losses[_])
+ax2.set_xlabel('Epoch index')
+ax2.set_ylabel('Loss')
+ax2.set_title('Training loss over epochs')
+ax2.grid(visible = True)
 
-accuracy_mean = round(sum(data["test"]["accuracy"]) / num_runs, 2)
-ax3 = fig.add_subplot(gs[0, 2])
-ax3.plot([_+1 for _ in range(num_runs)], data["test"]["accuracy"], '-', [_+1 for _ in range(num_runs)], [accuracy_mean]*num_runs, '--')
-ax3.set_xlabel('Run')
+# Evaluation accuracy
+accuracy = data["test"]["accuracy"]
+average_acc = round(sum(accuracy) / num_runs, 2)
+
+ax3.plot(runs, accuracy, '-', runs, [average_acc] * num_runs, '--')
+ax3.set_xlabel('Run index')
 ax3.set_ylabel('Accuracy (%)')
-ax3.set_title('Evaluation part')
+ax3.set_title('Model evaluation')
 ax3.set_ylim([50, 100])
-ax3.legend(['Measurments', f'Mean ({accuracy_mean}%)'])
+ax3.legend(['Measurments', f'Average ({average_acc}%)'])
+ax3.grid(visible = True)
 
-# Un long plot en bas à droite
-ax4 = fig.add_subplot(gs[1, 1:3])
+# Confusion matrix
+conf_matrix = data["test"]["confusion_matrix"]
 
+pos = ax4.matshow(conf_matrix)
+ax4.set_xlabel('Predicted label')
+ax4.set_ylabel('True label')
+ax4.set_xticks([_ for _ in range(7)], classes, rotation = 90)
+ax4.set_yticks([_ for _ in range(7)], classes)
+ax4.set_title(f'Confusion matrix on {num_runs} runs')
+fig.colorbar(pos, ax = ax4)
+
+for i in range(len(classes)):
+    for j in range(len(classes)):
+        c = conf_matrix[j][i]
+        ax4.text(i, j, str(c), va = 'center', ha = 'center', color = 'white')
+
+# Show plot 
+plt.title = data["hyperparameters"]["model"]
 plt.show()
